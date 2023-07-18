906a41479b876df8411457d2f334dc7af3139ca4
bacongubbe <94456001+bacongubbe@users.noreply.github.com>
2023-07-18 09:43:06 +0200

    added nifty animations

--- src/main/java/org/example/FemettanDice.java
+++ src/main/java/org/example/FemettanDice.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 
 public class FemettanDice {
 
+  private static final String[] FRAMES = new String[]{"⚀", "⚁", "⚂", "⚃", "⚄", "⚅"};
+
   public static void main(String[] args) {
-    String[] numbers = new String[]{ "1", "2", "3", "4", "★" };
+    String[] numbers = new String[]{"1", "2", "3", "4", "★"};
     Scanner scanner = new Scanner(System.in);
     String curr;
     while (true) {
@@ -14,7 +16,7 @@ public class FemettanDice {
       if (curr.equals("exit")) {
         break;
       }
-      if (curr.equals("help")){
+      if (curr.equals("help")) {
         System.out.println("""
           FEMETTAN DICE
 
@@ -23,16 +25,26 @@ public class FemettanDice {
           keklmao
 
           however, in order to kill the programme,
-          just enter 'exit' and then press the enter key.
+          type 'exit' and then press the enter key.
           This Kills The Crab. :)
           """);
         continue;
       }
-      if(!curr.isBlank()) {
-        System.out.print("enter 'help' for help, or 'exit' to kill the programme. Oh, and this is your result: ");
+      String message = "Rolling: ";
+      if (!curr.isBlank()) {
+        message = "Enter 'help' for help, or 'exit' to kill the programme. For now, let's roll the dice!: ";
       }
-      System.out.print(numbers[new Random().nextInt(numbers.length)]);
-      System.out.println();
+      int animationFrames = 20;
+      int animationDelayMs = 150;
+      for (int frame = 0; frame < animationFrames; frame++) {
+        System.out.print("\r" + message + FRAMES[frame % FRAMES.length]);
+        try {
+          Thread.sleep(animationDelayMs);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+      System.out.print("\r" + message + numbers[new Random().nextInt(numbers.length)] + "\n");
     }
   }
 }
